27e39df55ddb5dcbebf18d60e4adc413c29ae856
BalyshenAlexey <balyshen.aleksei@yandex.ru>
2022-01-23 13:01:28 +0700

    Реализован класс Range и Main для Range

--- Range/src/ru/academits/balyshen/range_main/Main.java
+++ Range/src/ru/academits/balyshen/range_main/Main.java
@@ -1,7 +1,40 @@
 package ru.academits.balyshen.range_main;
 
+import ru.academits.balyshen.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите начальное число диапазона:");
+        double startNumber = scanner.nextDouble();
+
+        System.out.println("Введите конечное число диапазона:");
+        double endNumber = scanner.nextDouble();
+
+        Range range = new Range(startNumber, endNumber);
+
+        System.out.printf("Длина диапазона равна: %.0f.%n", range.getLength());
+
+        System.out.println("Введите число диапазона:");
+        double number = scanner.nextDouble();
+
+        if (range.isInside(number)) {
+            System.out.printf("Введенное вами число попадает в диапазон от %f до %f.", range.getFrom(), range.getTo());
+        } else {
+            System.out.println("Введенное вами число не попадает в диапазон. Введите новое начальное число диапазона:");
+            range.setFrom(scanner.nextDouble());
+
+            System.out.println("Введите новое конечное число диапазона:");
+            range.setTo(scanner.nextDouble());
+
+            if (range.isInside(number)) {
+                System.out.printf("Введенное вами число попадает в новый диапазон от %f до %f.", range.getFrom(), range.getTo());
+            } else {
+                System.out.println("Введенное вами число не попадает в новый диапазон.");
+            }
+        }
     }
 }
