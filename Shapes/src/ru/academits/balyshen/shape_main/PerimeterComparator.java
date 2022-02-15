557b1d29fbf5e82c3948c350ff126dec0fe9a153
BalyshenAlexey <balyshen.aleksei@yandex.ru>
2022-02-15 21:21:44 +0700

    Исправлены компараторы

--- Shapes/src/ru/academits/balyshen/shape_main/PerimeterComparator.java
+++ Shapes/src/ru/academits/balyshen/shape_main/PerimeterComparator.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 public class PerimeterComparator implements Comparator<Shape> {
     @Override
     public int compare(Shape shape1, Shape shape2) {
-        return (int) Math.round(shape1.getPerimeter() - shape2.getPerimeter());
+        return Double.compare(shape1.getPerimeter(), shape2.getPerimeter());
     }
 }
 
