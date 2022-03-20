92a2722bd11e823e3894a3a017494cb08343bf96
BalyshenAlexey <balyshen.aleksei@yandex.ru>
2022-03-20 18:46:39 +0700

    доработан ArrayListHome

--- ArrayListHome/src/ru/academits/balyshen/arraylisthome/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/balyshen/arraylisthome/ArrayListHome.java
@@ -3,9 +3,14 @@ package ru.academits.balyshen.arraylisthome;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHome {
+    public static boolean isEven(Integer number) {
+        return number % 2 == 0;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome_input.txt"))) {
             ArrayList<String> lines = new ArrayList<>();
@@ -14,7 +19,17 @@ public class ArrayListHome {
                 lines.add(scanner.nextLine());
             }
 
-            System.out.println(lines);
+            System.out.println("Список строк из файла: " + lines);
         }
+
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+        for (Integer number : list1) {
+            if (isEven(number)) {
+                list1.remove(number);
+            }
+        }
+
+        System.out.println("Список четных чисел из списка 1: " + list1);
     }
 }
