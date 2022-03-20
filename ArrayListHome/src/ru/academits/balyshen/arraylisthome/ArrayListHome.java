33787b14011dad888265ccb34c3037a82c3a4aea
BalyshenAlexey <balyshen.aleksei@yandex.ru>
2022-03-20 17:47:31 +0700

    Добавлен модуль ArrayListHome

---
+++ ArrayListHome/src/ru/academits/balyshen/arraylisthome/ArrayListHome.java
@@ -0,0 +1,13 @@
+package ru.academits.balyshen.arraylisthome;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ArrayListHome {
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner fileScanner = new Scanner(new FileInputStream("input.txt"))) {
+
+        }
+    }
+}
