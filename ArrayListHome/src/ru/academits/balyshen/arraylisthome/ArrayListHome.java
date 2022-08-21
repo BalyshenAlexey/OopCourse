949cb47b05108bd72b587fce70ee7a441b4a96dc
BalyshenAlexey <balyshen.aleksei@yandex.ru>
2022-08-21 15:23:14 +0700

    Добавлен класс ArrayListHome

--- ArrayListHome/src/ru/academits/balyshen/arraylisthome/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/balyshen/arraylisthome/ArrayListHome.java
@@ -12,7 +12,7 @@ public class ArrayListHome {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome_input.txt"))) {
+        try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome\\ArrayListHome_input.txt"))) {
             ArrayList<String> lines = new ArrayList<>();
 
             while (scanner.hasNextLine()) {
@@ -22,14 +22,34 @@ public class ArrayListHome {
             System.out.println("Список строк из файла: " + lines);
         }
 
-        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        System.out.println("Список 1: " + list1);
 
-        for (Integer number : list1) {
-            if (isEven(number)) {
-                list1.remove(number);
+        list1.removeIf(ArrayListHome::isEven);
+        System.out.println("Список 1 после удаления четных чисел: " + list1);
+
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5, 1));
+        System.out.println("Список 2: " + list2);
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        boolean isNotRepeat;
+
+        for (int i = 0; i < list2.size(); i++) {
+            isNotRepeat = true;
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (list2.get(i).equals(list2.get(j))) {
+                    isNotRepeat = false;
+                    break;
+                }
+            }
+
+            if (isNotRepeat) {
+                list3.add(list2.get(i));
             }
         }
 
-        System.out.println("Список четных чисел из списка 1: " + list1);
+        System.out.println("Список 3: " + list3);
     }
 }
